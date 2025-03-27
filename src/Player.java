c35b1476cecc8ec4da5d05194c44f72d60dccf7f
30204504 <mahamomar.mir@ucalgary.ca>
2025-03-27 13:34:03 -0600

    wrote some skeleton methods and added some fields for player class

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,20 @@
 public class Player {
+
+
+    public String username;
+    public String email;
+    public String password;
+    private Ranking rank;
+
+    public Player(String username) {
+        this.username = username;
+    }
+
+    public void updateEmail(String username, String email) {
+        //
+    }
+
+    public void updatePassword(String username, String password) {
+        //
+    }
 }
