f088c3e59b56414a33937ed7e0fcc0c25ffd5e1f
30212688 <wissal.chibani@ucalgary.ca>
2025-03-28 16:41:32 -0600

    Defined instance variable and created the constructor to initialize the player in the PlayerStats class

--- src/PlayerStats.java
+++ src/PlayerStats.java
@@ -1,2 +1,9 @@
 public class PlayerStats {
+    // defining instance variable
+    private Player player;
+
+    // creating the constructor to initialize the player in the PlayerStats class
+    public PlayerStats(Player player) {
+        this.player = player;
+    }
 }
