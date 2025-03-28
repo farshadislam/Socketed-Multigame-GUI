c92aacdb5653597e26e17d50e17cf30e62a94e91
30212275 <harris.jan@ucalgary.ca>
2025-03-28 11:06:45 -0600

    Created fields and constructors specific to tic tac toe stats.

--- src/leaderboard_matchmaking/ticTacToeStats.java
+++ src/leaderboard_matchmaking/ticTacToeStats.java
@@ -1,57 +1,14 @@
 package leaderboard_matchmaking;
 
-public class ticTacToeStats {
-    private String playerId;
-    private int gamesPlayed;
-    private int wins;
-    private int losses;
-    private int ties;
-    private int mmr;
-    private Rank rank;
+public class ticTacToeStats extends GeneralStats{
+    private static final int MAX_MMR = 200;
+    private static final int MIN_MMR = 0;
+    private static final int TOTAL_RANKS = 7;
+    private static final double RANK_STEP = (MAX_MMR - MIN_MMR) / (double) TOTAL_RANKS;
 
-    public ticTacToeStats(String playerId) {
-        this.playerId = playerId;
-        this.gamesPlayed = 0;
-        this.wins = 0;
-        this.losses = 0;
-        this.ties = 0;
-        this.mmr = 0;
-        this.rank = Rank.BRONZE;
-    }
-
-    public void updateStats(boolean win, boolean tie) {
-        gamesPlayed++;
-        if (win) {
-            wins++;
-            // mmr gain logic
-        }
-        else if (tie) {
-            ties++;
-            // mmr tie logic
-        }
-        else {
-            losses++;
-            // mmr lose logic
-        }
-        updateRank();
-    }
-
-    private void updateRank() {
-        // mmr thresholds to update rank
-        // (if mmr > x) rank = Rank.x
-    }
-
-    public int getGamesPlayed() { return gamesPlayed; }
-    public int getWins() { return wins; }
-    public int getLosses() { return losses; }
-    public int getTies() { return ties; }
-    public int getMMR() { return mmr; }
-    public Rank getRank() { return rank; }
-
-    @Override
-    public String toString() {
-        return "TicTacToeStats -> Games: " + gamesPlayed + ", Wins: " + wins +
-                ", Losses: " + losses + ", Ties: " + ties +
-                ", MMR: " + mmr + ", Rank: " + rank;
+    public ticTacToeStats(String playerID) {
+        super(playerID);
+        // Initialize Tic Tac Toe–specific MMR.
+        this.tictactoemmr = 0;
     }
 }
