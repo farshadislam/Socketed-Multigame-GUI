35dd38e1c4b5ce45976631111d46422126dafeb9
30212275 <harris.jan@ucalgary.ca>
2025-03-28 09:59:44 -0600

    Created a method recordResult to record results based on a win, tie or loss and updates rank accordingly.

--- src/leaderboard_matchmaking/GeneralStats.java
+++ src/leaderboard_matchmaking/GeneralStats.java
@@ -9,6 +9,46 @@ public abstract class GeneralStats {
     protected int mmr;
     protected Rank rank;
 
+    public void stats (String playerID){
+        this.playerID = playerID;
+        this.gamesPlayed = 0;
+        this.wins = 0;
+        this.losses = 0;
+        this.ties = 0;
+        this.mmr = 0;               // Default starting mmr
+        this.rank = Rank.BRONZE;    // Default starting rank
+    }
 
+    /*
+    Recording game results.
+    If win is true, game was won
+    If tie is true, game ended in a tie
+    Otherwise, it was a loss
+     */
+    public void recordResult(boolean win, boolean tie){
+        gamesPlayed++;
+        if (win){
+            wins++;
+            updateMMR(true);
+        }
+        else if (tie) {
+            ties++;
+            updateMMRTies();
+        }
+        updateRank();
+    }
+
+    // Abstract class for updating player's rank based on current mmr
+    protected void updateRank() {
+    }
+
+    // Default MMR update for a tie.
+    protected void updateMMRTies() {
+        // No change in mmr on a tie
+    }
+
+    // Abstract method for updating MMR based on game result
+    protected void updateMMR(boolean win) {
+    }
 }
 
