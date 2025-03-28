c6b7ec1efb0aa1c4dea471cb129b58f991ccabfb
30204504 <mahamomar.mir@ucalgary.ca>
2025-03-27 19:33:10 -0600

    updated player constructor

--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,18 @@
 public class Player {
-
+    public int playerID;
     public String username;
     public String email;
     public String password;
-    public Ranking rank;
-    public connect4Stats Connect4Stats;
-    public ticTacToeStats TicTacToeStats;
-    public checkerStats CheckerStats;
+    public Rank rank;
+    public Connect4Stats connect4Stats;
+    public TicTacToeStats ticTacToeStats;
+    public CheckerStats checkerStats;
 
     public Player(String username) {
         this.username = username;
+        this.connect4Stats = new Connect4Stats();
+        this.ticTacToeStats = new TicTacToeStats();
+        this.checkerStats = new CheckerStats();
     }
 
     public void updateEmail(String username, String email) {
@@ -18,5 +21,10 @@ public class Player {
 
     public void updatePassword(String username, String password) {
         //
+    }
+
+    public GeneralStats getStats(String gameType) {
+
+        return null;
     }
 }
