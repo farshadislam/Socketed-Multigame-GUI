15458ab1028a4a1624ca88e1eedd461e2b238408
30212275 <harris.jan@ucalgary.ca>
2025-03-28 10:33:56 -0600

    Created a method for updating MMR.

--- src/leaderboard_matchmaking/connect4Stats.java
+++ src/leaderboard_matchmaking/connect4Stats.java
@@ -8,7 +8,31 @@ public class connect4Stats extends GeneralStats {
 
     public connect4Stats(String playerID) {
         super(playerID);
-        // We start connect4mmr at the midpoint, or wherever you prefer.
-        this.connect4mmr = 0;  // or 100, etc.
+        // We start connect4mmr at the midpoint.
+        this.connect4mmr = 0;
     }
+
+    /**
+     * Update MMR for Connect 4 when a game ends.
+     * @param win true if the player won, false if the player lost
+     */
+    @Override
+    protected void updateMMR(boolean win) {
+        // we add one rank step on a win, and subtract half a rank step on a loss.
+        if (win) {
+            // Gain 1 rank step
+            connect4mmr += (int) Math.round(RANK_STEP);
+        } else {
+            // Lose half a rank step
+            connect4mmr -= (int) Math.round(RANK_STEP / 2.0);
+        }
+
+        // Enforce MMR boundaries so it doesn't go below MIN or above MAX
+        if (connect4mmr < MIN_MMR) {
+            connect4mmr = MIN_MMR;
+        } else if (connect4mmr > MAX_MMR) {
+            connect4mmr = MAX_MMR;
+        }
+    }
+
 }
