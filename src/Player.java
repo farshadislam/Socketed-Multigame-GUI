72a56894e69a94362ed4cecc6d942148b65f406a
30204504 <mahamomar.mir@ucalgary.ca>
2025-03-27 22:43:31 -0600

    added getters and setters

--- src/Player.java
+++ src/Player.java
@@ -11,6 +11,7 @@ public class Player {
     public Player(String username) {
         this.username = username;
         this.connect4Stats = new Connect4Stats();
+        this.rank = Rank.BRONZE;
         this.ticTacToeStats = new TicTacToeStats();
         this.checkerStats = new CheckerStats();
     }
@@ -18,6 +19,47 @@ public class Player {
     public void updateEmail(String username, String email) {
         //
     }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
 
     public void updatePassword(String username, String password) {
         //
