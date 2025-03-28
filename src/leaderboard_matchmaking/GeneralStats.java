14595c1b54fb67e773501dce4e346495f38781b1
30212275 <harris.jan@ucalgary.ca>
2025-03-28 09:47:38 -0600

    Set up the fields of the abstract class

---
+++ src/leaderboard_matchmaking/GeneralStats.java
@@ -0,0 +1,14 @@
+package leaderboard_matchmaking;
+
+public abstract class GeneralStats {
+    protected String playerID;
+    protected int gamesPlayed;
+    protected int wins;
+    protected int losses;
+    protected int ties;
+    protected int mmr;
+    protected Rank rank;
+
+
+}
+
