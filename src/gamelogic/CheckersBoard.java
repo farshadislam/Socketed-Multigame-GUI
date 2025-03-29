6aec00861608b723ccd06f8e1313de093bd5526d
hamzi <hamzi.eljundi@ucalgary.ca>
2025-03-29 13:24:38 -0600

    working on Checkers board

--- src/gamelogic/CheckersBoard.java
+++ src/gamelogic/CheckersBoard.java
@@ -5,8 +5,7 @@ public class CheckersBoard {
     public static final int BOARD_SIZE = 8;
 
     public enum Piece {
-        EMPTY, RED, BLACK, RED_KING, WHITE_KING, BLACK_KING
-
+        EMPTY, RED, BLACK, RED_KING, BLACK_KING
     }
 
     private Piece[][] board;
@@ -14,20 +13,41 @@ public class CheckersBoard {
     public CheckersBoard() {
         board = new Piece[BOARD_SIZE][BOARD_SIZE];
         initializeBoard();
-
     }
 
     private void initializeBoard() {
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int col = 0; col < BOARD_SIZE; col++) {
-                if ((row + col) % 2 == 1) {
-                    board[row][col] = Piece.BLACK;
-                } else if (row > 4) {
-                    board[row][col] = Piece.RED;
+                if ((row + col) % 2 == 1) { // only dark squares
+                    if (row < 3) {
+                        board[row][col] = Piece.BLACK;
+                    } else if (row > 4) {
+                        board[row][col] = Piece.RED;
+                    } else {
+                        board[row][col] = Piece.EMPTY;
+                    }
+                } else {
+                    board[row][col] = Piece.EMPTY;
                 }
-            } else {
-                board[row][col] = Piece.EMPTY;
             }
         }
     }
-}
+
+    public void printBoard() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                System.out.print(pieceToChar(board[row][col]) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private char pieceToChar(Piece piece) {
+        switch (piece) {
+            case RED: return 'r';
+            case BLACK: return 'b';
+            case RED_KING: return 'R';
+            case BLACK_KING: return 'B';
+            default: return '.';
+        }
+    }
