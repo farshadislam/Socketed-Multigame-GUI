9991f0e791c3a87b45338db4c7bc4f86daeeff05
hamzi <hamzi.eljundi@ucalgary.ca>
2025-03-29 13:21:49 -0600

    working on Checkers board

--- src/gamelogic/CheckersBoard.java
+++ src/gamelogic/CheckersBoard.java
@@ -21,11 +21,30 @@ public class CheckersBoard {
                 if ((row + col) % 2 == 1) {
                     board[row][col] = Piece.BLACK;
                 } else if (row > 4) {
-                    board[row][col] = Piece.RED;
-                }
-            }  else {
-                board[row][col] = Piece.EMPTY;
+                     board[row][col] = Piece.RED;
+                 }
+                }  else {
+                   board[row][col] = Piece.EMPTY;
+                   }
+              }
+         }
+    }
+
+    public void printBoard() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+
             }
+            System.outprintln();
         }
     }
+
+    private char pieceToChar(Piece piece)
+        switch (piece) {
+    case RED: return 'r';
+    case BLACK: return 'b';
+    case RED_KING: return 'R';
+    case BLACK_KING: return 'B';
+    default: return '.';
+
 }
