63f786d5ab4483bce906930a9f6dce845ccb14fa
30169795 <brittney.ha@ucalgary.ca>
2025-03-30 11:33:46 -0600

    Added more methods to ConnectFourGame class based on class structure diagram

--- src/gamelogic/ConnectFourGame.java
+++ src/gamelogic/ConnectFourGame.java
@@ -2,6 +2,7 @@ package gamelogic;
 
 import java.util.List;
 
+// Do we need to extend or implement from GameGUI class?
 public class ConnectFourGame {
 
     public Board board;
@@ -20,10 +21,20 @@ public class ConnectFourGame {
         this.currentPlayer = players[0]; // first player starts, this may be changed to implement a RNG decision?
     }
 
-    public bool connectPlayers() {
-
+    // Manage player networking
+    public boolean connectPlayers() {
+        return true;
     }
 
+    public boolean disconnectPlayers() {
+        return true;
+    }
+
+    // Manage chat log (
+    public void sendMessage(String message) {
+    }
+
+    // need to integrate with GUI team (start/exit button)
     public void startGame() {
 
     }
@@ -31,5 +42,25 @@ public class ConnectFourGame {
     public void exitGame() {
 
     }
+
+    // restrict movements to column choice only. Returns true if move is made
+    public boolean makeMove(int col) {
+        // update board here so that the Connect Four piece is dropped in the correct column
+        // error handling: make sure the integer passed into this method is 0 <= col <= 6 (7 columns)
+        return true;
+    }
+
+    // made after move is made. We can also add a "skip turn" functionality if we have time
+    public void switchTurn() {
+
+    }
+
+    // called at end of game?
+    public Player checkWinner() {
+        Player winner = Player[0]; // placeholder. Can be Player[0] or Player[1]
+        return winner;
+    }
+
+
 }
 
