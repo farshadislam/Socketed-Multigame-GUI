4aa6967daa16137f92eab4cb7212eb268fb2707e
30211332 <hamna.asad@ucalgary.ca>
2025-03-29 23:15:27 -0600

    Started register() method along with verification of email

--- src/Authentication/LoginPage.java
+++ src/Authentication/LoginPage.java
@@ -1,10 +1,21 @@
 package Authentication;
 
+import java.util.ArrayList;
+
 public class LoginPage {
     private CredentialsDatabase database;
 
     public LoginPage(CredentialsDatabase database){
         this.database = database;
+    }
+
+    private enum State{
+        USERNAME_TAKEN,
+        EMPTY_USERNAME,
+        EMPTY_PASSWORD,
+        EMAIL_FORMAT_WRONG,
+        VERIFICATION_CODE_SENT,
+        REGISTRATION_SUCCESS,
     }
 
     /**
@@ -21,5 +32,48 @@ public class LoginPage {
             }
         }
         return null;
+    }
+
+
+    public State register(String username, String password, String email){
+        if(username.isEmpty()){
+            return State.EMPTY_USERNAME;
+        }
+        if(password.isEmpty()) {
+            return State.EMPTY_PASSWORD;
+        }
+        if(database.usernameLookup(username)){
+            return State.USERNAME_TAKEN;
+        }
+        if (!verifyEmailFormat(email)){
+            return State.EMAIL_FORMAT_WRONG;
+        }
+        Player newPlayer = new Player(username);
+        newPlayer.setPassword(password);
+        newPlayer.setEmail(email);
+        newPlayer.setRank(null);
+        newPlayer.setCheckersStats(null);
+        newPlayer.setConnect4Stats(null);
+        newPlayer.setTicTacToeStats(null);
+        //Yet to implement the code
+        return State.VERIFICATION_CODE_SENT;
+
+    }
+    private boolean verifyEmailFormat(String email){
+        if(!email.endsWith("@gmail.com") || email.indexOf("@") != email.lastIndexOf("@") || email.indexOf("@") <= 0){
+            return false;
+        }
+        String usernameOfEmail = email.substring(0, email.indexOf("@"));
+        for(int i = 0; i < usernameOfEmail.length(); i++){
+            char character = usernameOfEmail.charAt(i);
+            if(!(Character.isLetterOrDigit(character) || character == '.' || character == '_' || character == '-')){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void verifyEmailCode(String email, String code){
+        // Yet to implement the code
     }
 }
