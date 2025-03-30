3cfd30a08ad3380fe37da0e7860b57b1b64d368c
30174638 <kawthar.aroua1@ucalgary.ca>
2025-03-29 23:59:58 -0600

    tictactoe controller set up and testing template on scenebuilder

--- src/main/java/org/seng/gui/TicTacToeController.java
+++ src/main/java/org/seng/gui/TicTacToeController.java
@@ -4,6 +4,12 @@ import javafx.scene.control.Label;
 public class TicTacToeController {
     // game board
     // buttons
+    @FXML
+    private Label welcomeText;
+    protected void onHelloButtonClick() {
+        welcomeText.setText("   Welcome to TicTacToe!");
+    }
+
 
 
 
