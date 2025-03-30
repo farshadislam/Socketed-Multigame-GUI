811b05fc3cdef19a449cf19f82f9a0bdb0ffcbc4
30169795 <brittney.ha@ucalgary.ca>
2025-03-30 11:17:25 -0600

    Added constructor and methods to ConnectFourGame class

--- src/gamelogic/ConnectFourGame.java
+++ src/gamelogic/ConnectFourGame.java
@@ -1,18 +1,27 @@
 package gamelogic;
 
+import java.util.List;
+
 public class ConnectFourGame {
 
-    private Board board;
-    private Player[] players;  // 2 players
-    private Player currentPlayer;
-    private int gameID;
-    private String status;
+    public Board board;
+    public Player[] players;  // 2 players
+    public Player currentPlayer;
+    public int gameID;
+    public String status;
+    public List<String> chatLog;
 
     public ConnectFourGame(Board board, Player[] players, int gameID) {
         this.board = board;
         this.players = players;
         this.gameID = gameID;
-        this.status = "In Progress" // placeholder for status
+        this.status = "In Progress"; // placeholder for status
+        this.chatLog = new ArrayList<>();
+        this.currentPlayer = players[0]; // first player starts, this may be changed to implement a RNG decision?
+    }
+
+    public bool connectPlayers() {
+
     }
 
     public void startGame() {
