32f4f5c814994fc03a9d342262d5e5f0771124dc
30204504 <mahamomar.mir@ucalgary.ca>
2025-03-30 11:59:38 -0600

    updated methods in player and settings, added leaderboard methods, added comments

--- src/Authentication/Player.java
+++ src/Authentication/Player.java
@@ -3,11 +3,13 @@ import leaderboard_matchmaking.GeneralStats;
 import leaderboard_matchmaking.checkersStats;
 import leaderboard_matchmaking.connect4Stats;
 import leaderboard_matchmaking.ticTacToeStats;
-
+import leaderboard_matchmaking.Rank;
+import leaderboard_matchmaking.Leaderboard;
 import java.util.Objects;
 
 public class Player {
-    // create fields
+
+    // create fields for all the methods
     private String username;
     private String email;
     private String password;
@@ -16,35 +18,59 @@ public class Player {
     private ticTacToeStats TicTacToeStats;
     private checkersStats CheckersStats;
 
-    // create constructor
+    /**
+     * constructor for player class
+     * @param username username of the player
+     */
     public Player(String username) {
         this.username = username;
+        this.rank = Rank.BRONZE; // default rank
         this.Connect4Stats = new connect4Stats(username);
         this.TicTacToeStats = new ticTacToeStats(username);
         this.CheckersStats = new checkersStats(username);
     }
 
+    /**
+     * method for updating the email
+     * @param username username of the player
+     * @param newEmail new email they want to use
+     * @return true if email is updated, false otherwise
+     */
     public boolean updateEmail(String username, String newEmail) {
-        if (this.username.equals(username)){
-            if (!(newEmail==null) && !newEmail.isEmpty()){
+        if (this.username.equals(username)) {
+            if (!(newEmail == null) && !newEmail.isEmpty()) {
                 this.email = newEmail;
                 return true;
             }
         }
         return false;
     }
+
+    /**
+     *  method for updating the password
+     * @param username username for the player
+     * @param newPassword new password they want to use
+     * @return true if password is updated, false otherwise
+     */
     public boolean updatePassword(String username, String newPassword) {
-        if (this.username.equals(username)){
-            if (!(newPassword==null) && !newPassword.isEmpty()) {
+        if (this.username.equals(username)) {
+            if (!(newPassword == null) && !newPassword.isEmpty()) {
                 this.password = newPassword;
                 return true;
             }
         }
         return false;
     }
+
+    /**
+     * method for updating the username
+     * @param username username for the player
+     * @param newUsername new username they want to use
+     * @return true if username is updated, false otherwise
+     */
     public boolean updateUsername(String username, String newUsername) {
-        if (this.username.equals(username)){
-            if (!(newUsername==null) && !newUsername.isEmpty()) {
+        if (this.username.equals(username)) {
+            if (!(newUsername == null) && !newUsername.isEmpty()) {
                 this.username = newUsername;
                 return true;
             }
@@ -52,6 +78,8 @@ public class Player {
         return false;
     }
 
+
+    // setters and getters
     public connect4Stats getConnect4Stats() {
         return Connect4Stats;
     }
@@ -100,16 +128,28 @@ public class Player {
         this.password = password;
     }
 
-    public Rank getRank() {
-        return rank;
+    /**
+     * method for getting the rank
+     * @param game the type of game
+     * @return the rank
+     */
+    public Rank getRank(GameType game) {
+        GeneralStats stats = getStats(game.name().toLowerCase());
+        return (stats != null) ? stats.getRank() : Rank.BRONZE;
     }
 
     public void setRank(Rank rank) {
         this.rank = rank;
     }
 
+    /**
+     * method for getting the stats
+     * @param gameType the type of game
+     * @return the stats for the game
+     */
     public GeneralStats getStats(String gameType) {
         return switch (gameType.toLowerCase()) {
+            // change
             case "connect4" -> Connect4Stats;
             case "tictactoe" -> TicTacToeStats;
             case "checkers" -> CheckersStats;
@@ -118,13 +158,26 @@ public class Player {
 
     }
 
+    /**
+     * method for getting the total wins
+     * @return the total wins of all 3 games
+     */
+    public int getTotalWins(){
+        return Connect4Stats.getWins() + TicTacToeStats.getWins() + CheckersStats.getWins();
+    }
+
+    /**
+     * override method for equals
+     * @param object the object being used for comparing
+     * @return true if its equal, false otherwise
+     */
     @Override
     public boolean equals(Object object) {
-        if (this == object){
+        if (this == object) {
             return true;
             // if both references point to the same object (itself), true
         }
-        if (object == null || getClass() != object.getClass()){
+        if (object == null || getClass() != object.getClass()) {
             return false;
             // if one object is null or a different class (not a player), false
         }
--- src/Authentication/Settings.java
+++ src/Authentication/Settings.java
@@ -2,7 +2,7 @@ package Authentication;
 
 public class Settings {
 
-    // create fields
+    // create fields for the methods
     Player player;
 
     CredentialsDatabase database;
@@ -23,12 +23,21 @@ public class Settings {
         this.database = database;
     }
 
-    // create constructor
+    /**
+     * constructor for settings class
+     * @param player the player using the platform
+     * @param database the database with all the player information
+     */
     public Settings(Player player, CredentialsDatabase database){
         this.player = player;
         this.database = database;
     }
 
+    /**
+     * method for deleting account
+     * @param password the password for the account
+     * @return true if the account is deleted, false otherwise
+     */
     public boolean deleteAccount(String password){
         if (player.getPassword().equals(password)){
             return database.deleteExistingPlayer(player.getUsername());
@@ -36,6 +45,11 @@ public class Settings {
         return false;
     }
 
+    /**
+     * method for changing username
+     * @param newUsername new username they want to use
+     * @return true if the username is changed, false otherwise
+     */
     public boolean changeUsername(String newUsername){
         if (newUsername!=null && !(newUsername.isEmpty())){
             player.setUsername(newUsername);
@@ -44,6 +58,11 @@ public class Settings {
         return false;
     }
 
+    /**
+     * method for changing email
+     * @param newEmail new email they want to use
+     * @return true if the email is changed, false otherwise
+     */
     public boolean changeEmail(String newEmail) {
         if (newEmail!=null && !(newEmail.isEmpty())){
             player.setEmail(newEmail);
@@ -51,6 +70,12 @@ public class Settings {
         return false;
     }
 
+    /**
+     * method for changing password
+     * @param password current password being used
+     * @param newPassword new password they want to use
+     * @return true if the password was changed, false otherwise
+     */
     public boolean changePassword(String password, String newPassword) {
         if (player.getPassword().equals(password)) {
             if (newPassword != null && !(newPassword.isEmpty())) {
@@ -65,6 +90,7 @@ public class Settings {
 //        return new LoginPage(database);
 //    }
 
+    // method for logging out
     public boolean logout(){
         database.saveDatabase();
         return true;
