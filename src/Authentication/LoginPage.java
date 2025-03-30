e09b9d1e40973e7cd02e8913117647888c5dba02
30211332 <hamna.asad@ucalgary.ca>
2025-03-29 22:22:24 -0600

    Implemented login() for the player

--- src/Authentication/LoginPage.java
+++ src/Authentication/LoginPage.java
@@ -6,16 +6,20 @@ public class LoginPage {
     public LoginPage(CredentialsDatabase database){
         this.database = database;
     }
-    public void login(String username, String password){
-        //(We will basically be doing a lookup in the database object like Player player =
-        //database.findPlayerByUsername(username) and then we will check if the player
-        //is not null (the username exists in the database and the password entered is the
-        //same as player.getPassword and everything is correct, we will create a new
-        //Homepage with the player and database as its parameter and return that, so
-        //basically the homepage will be associated to that player, or if not then we will
-        //return null. So the existence of homePage will determine if the login was
-        //successful or not. Something like:
-        //HomePage home = loginPage.login("player1", "password123");
-        //if (home != null) { System.out.println("Login successful!"); )
+
+    /**
+     * Implements login for a player
+     * @param username The username of the player
+     * @param password The password of the player
+     * @return Homepage associated with the player
+     */
+    public HomePage login(String username, String password){
+        if(database.usernameLookup(username)){
+            Player player = database.findPlayerByUsername(username);
+            if (player.getPassword().equals(password)){
+                return new HomePage(player, database);
+            }
+        }
+        return null;
     }
 }
