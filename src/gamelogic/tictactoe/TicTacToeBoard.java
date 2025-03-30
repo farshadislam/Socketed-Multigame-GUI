89a328fe06feaa4334355546b78602219418b1d5
30178008 <kallista.sieu@ucalgary.ca>
2025-03-30 16:29:06 -0600

    Initialized TicTacToe board code

---
+++ src/gamelogic/tictactoe/TicTacToeBoard.java
@@ -0,0 +1,18 @@
+package gamelogic;
+
+public class TicTacToeBoard {
+
+    public static final int SIZE = 3;
+
+    public enum Mark {
+        EMPTY, X, O
+    }
+
+    private Mark[][] grid;
+
+    public TicTacToeBoard() {
+        grid = new Mark[SIZE][SIZE];
+        initializeBoard();
+    }
+
+}
