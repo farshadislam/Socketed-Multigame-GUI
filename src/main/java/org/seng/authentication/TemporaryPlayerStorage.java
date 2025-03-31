254a865916157104bbc70013216b3a6a248cbca2
30211332 <hamna.asad@ucalgary.ca>
2025-03-31 17:01:18 -0600

    Added temporary player storage class

---
+++ src/main/java/org/seng/authentication/TemporaryPlayerStorage.java
@@ -0,0 +1,22 @@
+package org.seng.authentication;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TemporaryPlayerStorage {
+    private static Map<String, Player> unverifiedPlayers = new HashMap<>();
+
+    // Add a player to temporary storage
+    public static void addPlayer(String username, Player player) {
+        unverifiedPlayers.put(username, player);
+    }
+
+    // Get a player from temporary storage
+    public static Player getPlayer(String username) {
+        return unverifiedPlayers.get(username);
+    }
+
+    // Remove a player from temporary storage
+    public static void removePlayer(String username) {
+        unverifiedPlayers.remove(username);
+    }
+}
