f2395d5fb0d14d4f524f248882a6e0299ef5fe7a
30174638 <kawthar.aroua1@ucalgary.ca>
2025-03-30 23:56:03 -0600

    basic stubs added to tictactoe controlle

--- src/main/java/org/seng/gui/TicTacToeController.java
+++ src/main/java/org/seng/gui/TicTacToeController.java
@@ -6,11 +6,25 @@ public class TicTacToeController {
     // buttons
     @FXML
     private Label welcomeText;
+
+
+    @FXML
+    public void initialize() {
+        System.out.println("Game initialized");
+    }
+
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("   Welcome to TicTacToe!");
     }
 
+    @FXML
+    protected void resetGame() {
+        welcomeText.setText("Welcome to TicTacToe!");
+        System.out.println("Game reset");
 
+    }
 
 
 }
