f821d8b15aeccd9c9cefd70f67e1bf1aeaf103be
30174638 <kawthar.aroua1@ucalgary.ca>
2025-04-01 01:47:45 -0600

    code integrated with controller and fxml (still needs work)

--- src/main/java/org/seng/gui/TicTacToeController.java
+++ src/main/java/org/seng/gui/TicTacToeController.java
@@ -1,29 +1,73 @@
 package org.seng.gui;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
 public class TicTacToeController {
-    // game board
-    // buttons
-    @FXML
-    private Label welcomeText;
-
 
     @FXML
-    public void initialize() {
-        System.out.println("Game initialized");
+    private Button button1;
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button3;
+
+    @FXML
+    private Button button4;
+
+    @FXML
+    private Button button5;
+
+    @FXML
+    private Button button6;
+
+    @FXML
+    private Button button7;
+
+    @FXML
+    private Button button8;
+
+    @FXML
+    private Button button9;
+
+
+    private int playerTurn = 0; // track if x or o
+    ArrayList<Button> buttons; // so they can be called easily
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        buttons = new ArrayList<>(Arrays.asList(button1,button2,button3,button4,button5,button6,button7,button8,button9));
+
+        buttons.forEach(button ->{
+            setupButton(button);
+            button.setFocusTraversable(false);
+        });
+
     }
 
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("   Welcome to TicTacToe!");
+    private void setupButton(Button button) {
+        button.setOnMouseClicked(mouseEvent -> {
+//            setPlayerSymbol(button);
+//            button.setDisable(true);
+//            checkIfGameIsOver();
+        });
     }
 
-    @FXML
-    protected void resetGame() {
-        welcomeText.setText("Welcome to TicTacToe!");
-        System.out.println("Game reset");
-
+    public void setPlayerSymbol(Button button){
+        if(playerTurn % 2 == 0){
+            button.setText("X");
+            playerTurn = 1;
+        } else{
+            button.setText("O");
+            playerTurn = 0;
+        }
     }
 
 
