b633022d57aa9d6543932c311d3c3cac5042b61f
30074044 <talha.zafar1@ucalgary.ca>
2025-04-01 00:35:30 -0600

    added new fonts for dashboard

--- src/main/java/org/seng/gui/HelloApplication.java
+++ src/main/java/org/seng/gui/HelloApplication.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import javafx.scene.text.Font;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Font.loadFont(getClass().getResourceAsStream("/org/seng/gui/fonts/Monoton-Regular.ttf"), 64);
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome-page.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 450);
 
