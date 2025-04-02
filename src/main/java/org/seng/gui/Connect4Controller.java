3524cb59ad719a0058dae15919b8226618f3cc7e
30206799 <hira.asad@ucalgary.ca>
2025-04-01 20:32:10 -0600

    Integrated game chat into connect 4 page

--- src/main/java/org/seng/gui/Connect4Controller.java
+++ src/main/java/org/seng/gui/Connect4Controller.java
@@ -1,14 +1,64 @@
 package org.seng.gui;
 
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
 
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 public class Connect4Controller {
-    @FXML
-    private Label welcomeText;
+    @FXML public void initialize() {}
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("   Welcome to Connect4! Make a four in a row to win!");
+    private void openChat() {
+        Stage chatStage = new Stage();
+        chatStage.setTitle("In-Game Chat");
+
+        VBox chatBox = new VBox(10);
+        chatBox.setPadding(new javafx.geometry.Insets(10));
+        chatBox.getStyleClass().add("chat-window");
+
+        TextArea chatDisplay = new TextArea();
+        chatDisplay.setEditable(false);
+        chatDisplay.setWrapText(true);
+        chatDisplay.setPrefHeight(200);
+        chatDisplay.getStyleClass().add("chat-display");
+
+        TextField messageField = new TextField();
+        messageField.setPromptText("Type your message...");
+        messageField.getStyleClass().add("chat-input");
+
+        Button sendButton = new Button("Send");
+        sendButton.getStyleClass().add("chat-send-button");
+
+        sendButton.setOnAction(e -> {
+            String msg = messageField.getText().trim();
+            if (!msg.isEmpty()) {
+                chatDisplay.appendText("You: " + msg + "\n");
+                messageField.clear();
+            }
+        });
+
+        messageField.setOnAction(e -> sendButton.fire());
+        chatBox.getChildren().addAll(chatDisplay, messageField, sendButton);
+
+        Scene scene = new Scene(chatBox, 350, 300);
+        scene.getStylesheets().add(getClass().getResource("gameChat.css").toExternalForm());
+        chatStage.setScene(scene);
+        chatStage.show();
     }
 }
+
