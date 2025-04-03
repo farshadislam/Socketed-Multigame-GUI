c03120c2cfdb581503f5f212e2d6d5181d8c02ae
30204504 <mahamomar.mir@ucalgary.ca>
2025-04-02 22:22:31 -0600

    settings test class, change username

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -51,3 +51,4 @@ public class PlayerTest {
         assertEquals(player, p2);
     }
 }
+
--- test/SettingsTest.java
+++ test/SettingsTest.java
@@ -1,17 +1,25 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.seng.authentication.CredentialsDatabase;
 import org.seng.authentication.Player;
+import org.seng.authentication.Settings;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SettingsTest {
     private Player player; // create player object for testing
+    private Settings settings;
 
     @BeforeEach
-    public void initializePlayer(){
+    public void initializeFields(){
         player = new Player("user", "user@gmail.com", "passWORD");
+        settings = new Settings(player, new CredentialsDatabase());
     }
 
     @Test
     public void testChangeUsername(){
-
+        assertTrue(settings.changeUsername("superUser"));
+        assertEquals("superUser", player.getUsername());
+        assertFalse(settings.changeUsername(null));
     }
 }
