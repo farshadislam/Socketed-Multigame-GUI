fc05cfaa4a6f2ce9c12009efad97d8aa17a3acc3
30204504 <mahamomar.mir@ucalgary.ca>
2025-04-02 19:35:18 -0600

    creating player tests, general test and before each

--- test/mainTest.java
+++ test/mainTest.java
@@ -2,8 +2,18 @@ import org.junit.jupiter.api.Test;
 
 public class mainTest {
 
+    private Player player; // create player object for testing
+
+    @BeforeEach
+    public void initializePlayer() {
+        player = new Player("user", "user@gmail.com", "passWORD");
+    }
+
     @Test
-    public void TestHi(){
+    public void testGeneral(){
+        assertEquals("user", player.getUsername());
+        assertEquals("user@gmail.com", player.getEmail());
+        assertNotEquals("password", player.getPassword());
 
     }
 }
