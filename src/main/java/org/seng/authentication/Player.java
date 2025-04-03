ca9fec1d60cb92623d34d2c261a240e214d8bf51
30204504 <mahamomar.mir@ucalgary.ca>
2025-04-03 15:36:46 -0600

    fixed errors and added comments

--- src/main/java/org/seng/authentication/Player.java
+++ src/main/java/org/seng/authentication/Player.java
@@ -9,7 +9,6 @@ import org.seng.authentication.Rank;
 import java.util.Objects;
 
 public class Player {
-
     // create fields for all the methods
     private String username;
     private String email;
@@ -24,12 +23,21 @@ public class Player {
     /**
      * Constructor that also sets initial wins for each game.
      * The 'rank' parameter is applied to each game’s rank as a default.
-     * @param username username of the player
+     *
+     * @param username      username of the player
+     * @param email         email of the player
+     * @param password      password of the player
+     * @param symbol        assigns player a colour
+     * @param rank          palyer's rank
+     * @param connect4Wins  wins in the connect4 game
+     * @param tictactoeWins wins in the tictactoe game
+     * @param checkersWins  wins in the checkers game
      */
-    public Player(String username, String email, String password, String rank, int connect4Wins, int tictactoeWins, int checkersWins) {
+    public Player(String username, String email, String password, char symbol, String rank, int connect4Wins, int tictactoeWins, int checkersWins) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.symbol = symbol;
         // Fall back on "BRONZE" if rank is null or blank
         if (rank == null || rank.trim().isEmpty()) {
             rank = "BRONZE";
@@ -46,11 +54,11 @@ public class Player {
         this.Connect4Stats.setRank(Rank.valueOf(rank));
         this.TicTacToeStats.setRank(Rank.valueOf(rank));
         this.CheckersStats.setRank(Rank.valueOf(rank));
-
     }
 
     /**
      * method for updating the email
+     *
      * @param username username of the player
      * @param newEmail new email they want to use
      * @return true if email is updated, false otherwise
@@ -66,8 +74,9 @@ public class Player {
     }
 
     /**
-     *  method for updating the password
-     * @param username username for the player
+     * method for updating the password
+     *
+     * @param username    username for the player
      * @param newPassword new password they want to use
      * @return true if password is updated, false otherwise
      */
@@ -83,7 +92,8 @@ public class Player {
 
     /**
      * method for updating the username
-     * @param username username for the player
+     *
+     * @param username    username for the player
      * @param newUsername new username they want to use
      * @return true if username is updated, false otherwise
      */
@@ -157,6 +167,7 @@ public class Player {
 
     /**
      * Return the rank from that game’s stats
+     *
      * @param game the type of game
      * @return the rank
      */
@@ -169,15 +180,17 @@ public class Player {
         this.rank = rank; // If you keep a “global” rank
     }
 
-    public void setVerificationCode(String verificationCode){
+    public void setVerificationCode(String verificationCode) {
         this.verificationCode = verificationCode;
     }
-    public String getVerificationCode(){
+
+    public String getVerificationCode() {
         return this.verificationCode;
     }
 
     /**
      * method for getting the stats
+     *
      * @param gameType the type of game
      * @return the stats for the game
      */
@@ -193,59 +206,135 @@ public class Player {
 
     // Total statistics
 
+    /**
+     * method for getting the total losses
+     *
+     * @return the total losses of all 3 games
+     */
     public int getTotalLosses() {
         return Connect4Stats.get_losses() + TicTacToeStats.get_losses() + CheckersStats.get_losses();
     }
 
+    /**
+     * method for getting the total games played
+     *
+     * @return the total games played of all 3 games
+     */
     public int getTotalGamesPlayed() {
         return Connect4Stats.getGamesPlayed() + TicTacToeStats.getGamesPlayed() + CheckersStats.getGamesPlayed();
     }
 
+    /**
+     * method for getting the total ties
+     *
+     * @return the total ties of all 3 games
+     */
     public int getTotalTies() {
         return Connect4Stats.get_ties() + TicTacToeStats.get_ties() + CheckersStats.get_ties();
     }
 
     /**
      * method for getting the total wins
+     *
      * @return the total wins of all 3 games
      */
-    public int getTotalWins(){
+    public int getTotalWins() {
         return Connect4Stats.getWins() + TicTacToeStats.getWins() + CheckersStats.getWins();
+    }
+
+    // Connect4 methods:
+    public int getConnect4Wins() {
+        return Connect4Stats.get_wins();
+    }
+
+    public int getConnect4Losses() {
+        return Connect4Stats.get_losses();
+    }
+
+    public int getConnect4GamesPlayed() {
+        return Connect4Stats.getGamesPlayed();
+    }
+
+    public void setConnect4Wins(int w) {
+        Connect4Stats.setWins(w);
+    }
+
+    public void setConnect4Losses(int l) {
+        Connect4Stats.setLosses(l);
+    }
+
+    public void setConnect4GamesPlayed(int gp) {
+        Connect4Stats.setGamesPlayed(gp);
+    }
+
+    public void setConnect4Ties(int t) {
+        Connect4Stats.setTies(t);
+    }
+
+    // TicTacToe methods:
+    public int getTicTacToeWins() {
+        return TicTacToeStats.get_wins();
+    }
+
+    public int getTicTacToeLosses() {
+        return TicTacToeStats.get_losses();
+    }
+
+    public int getTicTacToeGamesPlayed() {
+        return TicTacToeStats.getGamesPlayed();
+    }
+
+    public void setTicTacToeWins(int w) {
+        TicTacToeStats.setWins(w);
+    }
+
+    public void setTicTacToeLosses(int l) {
+        TicTacToeStats.setLosses(l);
+    }
+
+    public void setTicTacToeGamesPlayed(int gp) {
+        TicTacToeStats.setGamesPlayed(gp);
+    }
+
+    public void setTicTacToeTies(int t) {
+        TicTacToeStats.setTies(t);
+    }
+
+    // Checkers methods:
+    public int getCheckersWins() {
+        return CheckersStats.get_wins();
+    }
+
+    public int getCheckersLosses() {
+        return CheckersStats.get_losses();
+    }
+
+    public int getCheckersGamesPlayed() {
+        return CheckersStats.getGamesPlayed();
+    }
+
+    public void setCheckersWins(int w) {
+        CheckersStats.setWins(w);
+    }
+
+    public void setCheckersLosses(int l) {
+        CheckersStats.setLosses(l);
+    }
+
+    public void setCheckersGamesPlayed(int gp) {
+        CheckersStats.setGamesPlayed(gp);
+    }
+
+    public void setCheckersTies(int t) {
+        CheckersStats.setTies(t);
     }
 
     /**
      * override method for equals
+     *
      * @param object the object being used for comparing
      * @return true if its equal, false otherwise
      */
-    // Connect4 methods:
-    public int getConnect4Wins()         { return Connect4Stats.get_wins(); }
-    public int getConnect4Losses()       { return Connect4Stats.get_losses(); }
-    public int getConnect4GamesPlayed()  { return Connect4Stats.getGamesPlayed(); }
-    public void setConnect4Wins(int w)   { Connect4Stats.setWins(w); }
-    public void setConnect4Losses(int l) { Connect4Stats.setLosses(l); }
-    public void setConnect4GamesPlayed(int gp) { Connect4Stats.setGamesPlayed(gp); }
-    public void setConnect4Ties(int t)   { Connect4Stats.setTies(t); }
-
-    // TicTacToe methods:
-    public int getTicTacToeWins()        { return TicTacToeStats.get_wins(); }
-    public int getTicTacToeLosses()      { return TicTacToeStats.get_losses(); }
-    public int getTicTacToeGamesPlayed() { return TicTacToeStats.getGamesPlayed(); }
-    public void setTicTacToeWins(int w)  { TicTacToeStats.setWins(w); }
-    public void setTicTacToeLosses(int l){ TicTacToeStats.setLosses(l); }
-    public void setTicTacToeGamesPlayed(int gp) { TicTacToeStats.setGamesPlayed(gp); }
-    public void setTicTacToeTies(int t)  { TicTacToeStats.setTies(t); }
-
-    // Checkers methods:
-    public int getCheckersWins()         { return CheckersStats.get_wins(); }
-    public int getCheckersLosses()       { return CheckersStats.get_losses(); }
-    public int getCheckersGamesPlayed()  { return CheckersStats.getGamesPlayed(); }
-    public void setCheckersWins(int w)   { CheckersStats.setWins(w); }
-    public void setCheckersLosses(int l) { CheckersStats.setLosses(l); }
-    public void setCheckersGamesPlayed(int gp) { CheckersStats.setGamesPlayed(gp); }
-    public void setCheckersTies(int t)   { CheckersStats.setTies(t); }
-
-
     @Override
     public boolean equals(Object object) {
         if (this == object) {
@@ -302,6 +391,5 @@ public class Player {
                 String.valueOf(CheckersStats.getMMR())
         );
     }
-
-
 }
+
