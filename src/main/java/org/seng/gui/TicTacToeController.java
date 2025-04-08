64d644b560c2b5feed695b46b96a008070b64625
SyedOmar <syed.omar@ucalgary.ca>
2025-04-08 17:51:53 -0600

    Symbols can now show up in the tic tac toe gui window. Not perfected yet though.

--- src/main/java/org/seng/gui/TicTacToeController.java
+++ src/main/java/org/seng/gui/TicTacToeController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 
@@ -213,7 +215,13 @@ public class TicTacToeController {
             e.printStackTrace();
         }
     }
-
+    @FXML
+    private void handleButtonClick(ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
+        if (clickedButton.getText().isEmpty()) {
+            clickedButton.setText("X"); // Or "O" depending on game state
+        }
+    }
 }
 
 
