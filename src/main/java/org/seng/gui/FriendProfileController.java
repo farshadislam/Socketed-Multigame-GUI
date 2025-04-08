60e0db0c034c8c0e67cb642737cd6c504fcee70b
30174638 <kawthar.aroua1@ucalgary.ca>
2025-04-08 17:09:15 -0600

    mmr and rank logic for dummy data added will be changed later

--- src/main/java/org/seng/gui/FriendProfileController.java
+++ src/main/java/org/seng/gui/FriendProfileController.java
@@ -1,7 +1,5 @@
 package org.seng.gui;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,43 +7,36 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class FriendProfileController {
 
-    @FXML
-    private Label friendNameLabel;
-    @FXML
-    private Label friendLastOnlineLabel;
-    @FXML
-    private Label friendWinsLabel;
-    @FXML
-    private Label friendLossesLabel;
-    @FXML
-    private Label friendTiesLabel;
-    @FXML
-    private TableView<ProfilePageController.GameStat> friendGameStatsTable;
-    @FXML
-    private TableColumn<ProfilePageController.GameStat, String> friendGameColumn;
-    @FXML
-    private TableColumn<ProfilePageController.GameStat, Integer> friendGameWinsColumn;
-    @FXML
-    private TableColumn<ProfilePageController.GameStat, Integer> friendGameLossesColumn;
-    @FXML
-    private TableColumn<ProfilePageController.GameStat, Integer> friendGameTiesColumn;
-    @FXML
-    private ImageView backIcon;
+    @FXML private Label friendNameLabel;
+    @FXML private Label friendLastOnlineLabel;
+    @FXML private Label friendWinsLabel;
+    @FXML private Label friendLossesLabel;
+    @FXML private Label friendTiesLabel;
     @FXML private Label friendMatchesLabel;
 
+    @FXML private ImageView backIcon;
 
+    @FXML private TableView<ProfilePageController.GameStat> friendGameStatsTable;
+    @FXML private TableColumn<ProfilePageController.GameStat, String> friendGameColumn;
+    @FXML private TableColumn<ProfilePageController.GameStat, String> friendRankColumn;
+    @FXML private TableColumn<ProfilePageController.GameStat, Integer> friendMmrColumn;
+    @FXML private TableColumn<ProfilePageController.GameStat, Integer> friendGameWinsColumn;
+    @FXML private TableColumn<ProfilePageController.GameStat, Integer> friendGameLossesColumn;
+    @FXML private TableColumn<ProfilePageController.GameStat, Integer> friendGameTiesColumn;
 
     @FXML
     public void initialize() {
         friendGameColumn.setCellValueFactory(data -> data.getValue().gameProperty());
+        friendRankColumn.setCellValueFactory(data -> data.getValue().rankProperty());
+        friendMmrColumn.setCellValueFactory(data -> data.getValue().mmrProperty().asObject());
         friendGameWinsColumn.setCellValueFactory(data -> data.getValue().winsProperty().asObject());
         friendGameLossesColumn.setCellValueFactory(data -> data.getValue().lossesProperty().asObject());
         friendGameTiesColumn.setCellValueFactory(data -> data.getValue().tiesProperty().asObject());
+
         try {
             Image backImage = new Image(getClass().getResourceAsStream("/org/seng/gui/images/backicon.png"));
             backIcon.setImage(backImage);
@@ -54,8 +45,8 @@ public class FriendProfileController {
         }
 
         backIcon.setOnMouseClicked(event -> goBack());
-
     }
+
     public void setProfileData(String name, String lastOnline, int wins, int losses, int ties) {
         friendNameLabel.setText(name);
         friendLastOnlineLabel.setText(lastOnline);
@@ -67,10 +58,11 @@ public class FriendProfileController {
         friendMatchesLabel.setText(String.valueOf(totalMatches));
 
         friendGameStatsTable.setItems(FXCollections.observableArrayList(
-                new ProfilePageController.GameStat("Checkers", 3, 1, 1),
-                new ProfilePageController.GameStat("Tic Tac Toe", 2, 2, 0),
-                new ProfilePageController.GameStat("Connect 4", 1, 0, 2)
+                new ProfilePageController.GameStat("Checkers", "Silver", 3, 3, 1, 1),
+                new ProfilePageController.GameStat("Tic Tac Toe", "Gold", 2, 2, 2, 0),
+                new ProfilePageController.GameStat("Connect 4", "Bronze", 1, 1, 0, 2)
         ));
+
     }
 
     private void goBack() {
